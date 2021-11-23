3ed1618bce4aa8d31ff774839b5905662cb83e18
chandana-21 <chandanakuntala21@gmail.com>
2021-11-23 13:09:50 +0530

    edited

--- server/util.py
+++ server/util.py
@@ -1,31 +1,12 @@
-import json
 import pickle
+import json
 import numpy as np
 
 __locations = None
 __data_columns = None
 __model = None
 
-
-def get_location_names():
-    return __locations
-
-def load_saved_artifacts():
-    print("Loading saved artifacts...start")
-    global  __data_columns
-    global __locations
-
-    with open("/Users/chandana/Documents/TECH STACKS/AI:ML/House Price/server/artifacts/columns.json","r") as f:
-        __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[3:]
-
-    global __model
-
-    with open("/Users/chandana/Documents/TECH STACKS/AI:ML/House Price/server/artifacts/Bengaluru_House_Price.pickle", "rb") as f:
-        __model = pickle.load(f)
-    print("Loading saved artifacts...completed")
-
-def get_predicted_price(location,sqft,bhk,bath):
+def get_estimated_price(location,sqft,bhk,bath):
     try:
         loc_index = __data_columns.index(location.lower())
     except:
@@ -40,7 +21,31 @@ def get_predicted_price(location,sqft,bhk,bath):
 
     return round(__model.predict([x])[0],2)
 
-if __name__ == "__main__":
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global  __data_columns
+    global __locations
+
+    with open("/Users/chandana/Documents/GitHub/predict-house-price/server/artifacts/columns.json", "r") as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:] 
+
+    global __model
+    if __model is None:
+        with open('/Users/chandana/Documents/GitHub/predict-house-price/server/artifacts/Bengaluru_House_Price.pickle', 'rb') as f:
+            __model = pickle.load(f)
+    print("loading saved artifacts...done")
+
+def get_location_names():
+    return __locations
+
+def get_data_columns():
+    return __data_columns
+
+if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
-    print(get_predicted_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) 
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  
