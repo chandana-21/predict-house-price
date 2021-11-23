d815e5b8f15b5ed96a945488aa113c17e91a4842
chandana-21 <chandanakuntala21@gmail.com>
2021-11-23 13:09:46 +0530

    edited

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ import util
 
 app = Flask(__name__)
 
-@app.route('/get_location_names')
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
@@ -20,12 +20,13 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_predicted_price(location,total_sqft,bhk,bath)
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
-if __name__ ==  "__main__":
-    print("running")
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
     app.run()
